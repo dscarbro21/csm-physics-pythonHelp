52c17971448b33e9bc982bbc91097cb04550e7d5
Daniel Scarbrough <dscarbro@mines.edu>
2021-01-31 19:30:04 -0700

    Init commit with some basic code examples

---
+++ exampleCode/chapter1/helloWorldMain.py
@@ -0,0 +1,6 @@
+def main():
+    print("hello world")
+    print("I am in", __name__)
+
+if __name__ == "__main__":
+    main()
---
+++ exampleCode/chapter2/section2/printIntroEscapes.py
@@ -0,0 +1,23 @@
+def main():
+    print("Python has \"escape characters\"")
+    print("- which were used to print quotes in a string defined with quotes")
+
+    print()
+    print("Instead of empty prints, I can print newlines\n - with \\n")
+
+    print("\nI can put them\nanywhere\nin the string\n")
+
+    print("notice that \\ only affects the next character")
+
+    print("I can even change the end with these", end="\n\n")
+    print("or separate", "inputs with", "tabs", sep="\t")
+    print()
+
+    print("Adjust both \"sep\"", "and", "\"end\" as you wish", sep="\t", end="\n\n")
+
+    print("Common escape characters:")
+    print("\\n \\t \\\ \\' \\\"", end="\n\n")
+    print("Other more specialized ones also exist")
+
+if __name__ == "__main__":
+    main()
