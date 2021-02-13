c40af1d991ee21b746a3e36b6b272164656b6282
Daniel Scarbrough <dscarbro@mines.edu>
2021-02-13 16:32:49 -0700

    Tracking new directories

---
+++ docCode/chapter2/section1/opsIntro.py
@@ -0,0 +1,29 @@
+print("Defining our variables:")
+x = 6
+y = 4
+print(x, y)
+print("x + y =", x + y)
+
+z = x + y
+print("z = x + y =", z)
+
+z = x - 1
+print("z =", z)
+
+print("x * 2 =", x * 2)
+z = x * y
+print("z =", z)
+
+print("x / 2 =", x / 2)
+print("x / y =", x, "/", y, "=", x / y)
+
+print("store x / y in z..")
+z = x / y
+print("z =", z)
+print("z + 2 =", z + 2)
+print("z * 2 =", z * 2)
+
+print("\nx squared is x**2 =", x**2)
+print("the remainder of x / y is: x % y =", x % y)
+print("16 / x rounded down is: 16 // x =", 16 // x)
+print("x(y + z) =", x * (y + z))
---
+++ docCode/chapter2/section2/printIntroEscapes.py
@@ -0,0 +1,19 @@
+print("Python has \"escape characters\"")
+print("- which were used to print quotes in a string defined with quotes")
+
+print()
+print("Instead of empty prints, I can print newlines\n - with \\n")
+
+print("\nI can put them\nanywhere\nin the string\n")
+
+print("notice that \\ only affects the next character")
+
+print("I can even change the end with these", end="\n\n")
+print("or separate", "inputs with", "tabs", sep="\t")
+print()
+
+print("Adjust both \"sep\"", "and", "\"end\" as you wish", sep="\t", end="\n\n")
+
+print("Common escape characters:")
+print("\\n \\t \\\ \\' \\\"", end="\n\n")
+print("Other more specialized ones also exist")
